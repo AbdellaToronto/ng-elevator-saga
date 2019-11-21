be7984c8a87803fdee92dfe99f4e0068535889c7
AbdellaToronto <abdellaali@Abdellas-MacBook-Pro.local>
2019-11-21 14:33:19 -0500

    lots of progress but not done yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import './legacy-assets/base.js';
+import './legacy-assets/movable.js';
+import './legacy-assets/floor.js';
+import './legacy-assets/user.js';
+import './legacy-assets/elevator.js';
+import './legacy-assets/interfaces.js';
+import './legacy-assets/world.js';
+import './legacy-assets/presenters.js';
+import './legacy-assets/challenges.js';
+import './legacy-assets/fitness.js';
+import './legacy-assets/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./style.css', './app.component.css', '../../node_modules/codemirror/theme/solarized.css']
 })
 export class AppComponent {
   title = 'ng-elevator-saga';
